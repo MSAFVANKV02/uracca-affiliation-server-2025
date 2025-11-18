3a845e5ab17bf9663cc2fcd0129f52c22d9ce998
MUHAMMED SAFVAN KV <mskvphed@gmail.com>
2025-11-17 18:55:03 -0800

    bug fix 1

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -8,6 +8,10 @@ const JWT_SECRET_ADMIN = process.env.JWT_SECRET_ADMIN
 export const authenticateUser = async (req, res, next) => {
   try {
     const token = req.cookies?.aff_ses_server || req.headers.authorization?.split(" ")[1];
+    console.log(req,'req');
+
+    console.log(token,'token');
+    
 
     if (!token) {
       return res.status(401).json({ success: false, message: "Not authorized, no token found." });
